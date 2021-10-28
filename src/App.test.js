ab56cd6fcd409ad8bdf4d6c5355cce201fb45ea2
Carolina Adriana Hidalgo Avila <carolina.hidalgo@ucb.edu.bo>
2021-10-28 09:45:36 -0400

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,7 @@
+import sumar from "./App.js";
+
+describe("Sumar", () => {
+  it("deberia sumar dos numeros", () => {
+    expect(sumar(3, 2)).toEqual(5);
+  });
+});
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import sumar from "./App";
+
+const first = document.querySelector("#first-number");
+const second = document.querySelector("#second-number");
+const form = document.querySelector("#sumar-form");
+const div = document.querySelector("#resultado-div");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
