7b507c416e19960bf80b6905373245c105c84dcd
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-11-07 00:50:29 -0300

    INTERFAZ: Añadir descripcion a tarea

--- src/Tarea.js
+++ src/Tarea.js
@@ -1,10 +1,23 @@
 class Tarea{
-    constructor(titulo) {
+    constructor(titulo,descripcion,id) {
         this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.id = id;
+    }
+
+    getId(){
+        console.log("id tarea",this.id)
+        return this.id;
     }
 
     getTitulo(){
         return this.titulo;
     }
+
+    getDescripcion(){
+        return this.descripcion;
+    }
+
 }
 export default Tarea;
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import ListaTareas from "./ListaTareas.js";
-
+import Tarea from "./Tarea.js";
 let listaTareasAgregadas = new ListaTareas();
 
 const tarea = document.querySelector("#tarea");
 const listaTareas = document.querySelector("#lista-tareas");
 const form = document.querySelector("#agregarTareas-form");
+
+const descripcion = document.querySelector("#descripcionTarea");
+const dialogoDescripcion = document.querySelector("#dialogo-descripcion");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
@@ -12,7 +15,23 @@ form.addEventListener("submit", (event) => {
     listaTareasAgregadas = new ListaTareas();
   }
   let tituloTarea = tarea.value;
-  listaTareasAgregadas.agregarTarea(tituloTarea);
+  let desripcionTarea = descripcion.value;
+  listaTareasAgregadas.agregarTarea(tituloTarea,desripcionTarea);
   listaTareas.innerHTML = listaTareasAgregadas.getUlListaTareas();
+
+  const b = document.querySelectorAll(".button-descripcion"); 
+  b.forEach(btn => { 
+    btn.addEventListener("click", mostrarDescripcion(btn.id), false);
+  });
 });
 
+function mostrarDescripcion(idTarea){
+  console.log("CLICK",idTarea);
+  const dialogoDescripcion = document.querySelector("#dialogo-descripcion");
+  const tarea = listaTareasAgregadas.getTareaPorId(idTarea);
+  console.log("get",tarea)
+  dialogoDescripcion.innerHTML = tarea.getDescripcion();
+  //dialogoDescripcion.showModal();
+}
+
+export default mostrarDescripcion;
