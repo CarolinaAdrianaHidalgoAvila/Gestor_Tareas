578e915bf2973b17ab6b3d762f7b3d0e135e0a8a
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2021-11-08 16:21:08 -0400

    filtrar tarea por fecha limite - CC3 - devolver lista vacia si no se filtra ninguna fecha

--- src/filtrarTareaFecha.test.js
+++ src/filtrarTareaFecha.test.js
@@ -62,4 +62,26 @@ describe("Filtrar tarea por descripción", () => {
         listaTareas.agregarTarea("ir a piscina","nadar 50 minutos","","2022-07T00:00");
         expect(  listaTareas.filtrarPorUnRangoFechas("2022-11-07T13:59","2022-12-07T03:59")).toEqual(["tarea-1","tarea-2"]);
     });
+
+    // CC3
+    it("Deberia devolver lista vacia si no se filtra la fecha espeficificada", () => {
+        var listaTareas = new ListaTareas(); 
+        listaTareas.agregarTarea("ir a piscina","nadar 50 minutos","","2022-10-07T23:29");     
+        expect(  listaTareas.filtrarPorUnaFecha("2019-11-07T00:00")).toEqual([]);
+    }); 
+    it("Deberia devolver lista vacia si no se filtra la fecha espeficificada", () => {
+        var listaTareas = new ListaTareas(); 
+        listaTareas.agregarTarea("ir a piscina","nadar 50 minutos","","2022-10-07T23:29");
+        listaTareas.agregarTarea("ir a piscina","nadar 50 minutos","","2022-10-10T23:29"); 
+        listaTareas.agregarTarea("ir a piscina","nadar 50 minutos","","2022-10-11T23:29");      
+        expect(  listaTareas.filtrarPorUnaFecha("2022-10-12T23:29")).toEqual([]);
+    }); 
+    it("Deberia devolver lista vacia si no existe tarea en el rango de fechas espeficificadas", () => {
+        var listaTareas = new ListaTareas(); 
+        listaTareas.agregarTarea("ir a piscina","nadar 50 minutos","","2022-10-07T23:29");
+        listaTareas.agregarTarea("ir a piscina","nadar 50 minutos","","2022-10-10T23:29"); 
+        listaTareas.agregarTarea("ir a piscina","nadar 50 minutos","","2022-10-11T23:29");      
+        expect(  listaTareas.filtrarPorUnRangoFechas("2022-11-07T23:59","2022-12-07T23:59")).toEqual([]);
+    });
+
 });
